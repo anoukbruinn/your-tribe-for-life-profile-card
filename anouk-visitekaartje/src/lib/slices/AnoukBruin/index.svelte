805ff01012d9538178303685f92a626a547aa27c
anoukbruinn <112856687+anoukbruinn@users.noreply.github.com>
2023-09-08 14:13:00 +0200

    done

---
+++ anouk-visitekaartje/src/lib/slices/AnoukBruin/index.svelte
@@ -0,0 +1,18 @@
+<script>
+  /** @type {import("@prismicio/client").Content.AnoukBruinSlice} */
+  export let slice;
+</script>
+
+<section
+  data-slice-type={slice.slice_type}
+  data-slice-variation={slice.variation}
+>
+  Placeholder component for {slice.slice_type} (variation: {slice.variation})
+  Slices
+  <p>{slice.primary.title}</p>
+  
+</section>
+
+<pre>
+	{JSON.stringify(slice, null, 2)}
+</pre>
---
+++ anouk-visitekaartje/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+
+<script>
+    import { SliceZone } from '@prismicio/svelte'
+    import { components } from '$lib/slices' 
+    
+    export let data
+
+    console.log(data)
+</script>
+
+<!-- <h1>{ data.slices }</h1> -->
+ <h1>{ data.title }</h1>
+ <img src="{data.image.url}" width="50" alt="{data.image.alt}"> 
+
+<!-- <SliceZone slices={data.slices} {components} /> -->
+
+<style>
+
+    img {
+        width: 100px;
+        height: 100px;
+        border-radius: 100%;
+        margin-left: 30px;
+        border-color: orange;
+    }
+
+    h1 { 
+        text-align: right;
+    }
+</style>
